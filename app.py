883cfd00310735e620e5f7e269f5996c26e6db35
Renuka <raviravi11327@gmail.com>
2026-03-23 14:35:59 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,11 @@
+a= 10
+b = 30
+c=a+b
+if a==1:
+    print("saru is a mental")
+elif b==30:
+    print("yuva is a araloosu")
+elif c== 40:
+    print("renu is a angel")
+else:
+    print("ellam paithiyam than")
